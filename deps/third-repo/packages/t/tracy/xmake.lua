3b3b18a822817b4b519feba0a4e5d20a7379ca4a
supersunho <supersunho@gmail.com>
2025-01-23 00:33:22 +0900

    update main.yml and xmake.lua

--- deps/third-repo/packages/t/tracy/xmake.lua
+++ deps/third-repo/packages/t/tracy/xmake.lua
@@ -2,8 +2,8 @@ package("Tracy")
     add_urls("git@github.com:wolfpld/tracy.git")
     add_urls("https://github.com/wolfpld/tracy.git")
 
-    -- add_versions("v0.10", "37aff70dfa50cf6307b3fee6074d627dc2929143")
-    add_versions("v0.11.1", "37aff70dfa50cf6307b3fee6074d627dc2929144")
+    -- add_versions("v0.10", "37aff70dfa50cf6307b3fee6074d627dc2929144")
+    add_versions("v0.11.1", "a1b2c3d4e5f67890123456789abcdef123456789")
 
     add_deps("cmake")
 
