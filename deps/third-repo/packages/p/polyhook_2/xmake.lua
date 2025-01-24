f2a053536bc23a77df95da07dd3eea137bbe8c8c
supersunho <supersunho@gmail.com>
2025-01-24 18:08:50 +0900

    update 1 file and create 1 file

--- deps/third-repo/packages/p/polyhook_2/xmake.lua
+++ deps/third-repo/packages/p/polyhook_2/xmake.lua
@@ -4,7 +4,7 @@ package("polyhook_2")
     -- local version = os.iorun("git rev-list --count HEAD"):trim()
     -- git rev-list --count HEAD ==> 593 ==> 0.0.593
     add_versions("v0.0.593", "19e7cec8cce4a0068f6db04b6d3680c078183002") 
-    add_patches("v0.0.593", "for-arm64.patch")
+    add_patches("v0.0.593", path.join(os.scriptdir(), "patches", "polyhook_2", "for-arm64.patch")))
 
     set_sourcedir(os.scriptdir())
     
@@ -12,15 +12,39 @@ package("polyhook_2")
 
     on_install(function (package)
         local configs = {}
+
+        -- Set CMake Build types
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
-        if is_plat("linux") then
+        -- Set CMake Options for PolyHook 
+        table.insert(configs, "-DPOLYHOOK_BUILD_DLL=ON") 
+        table.insert(configs, "-DPOLYHOOK_BUILD_SHARED_LIB=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DPOLYHOOK_USE_EXTERNAL_ZYDIS=ON") 
+        table.insert(configs, "-DASMJIT_STATIC=" .. (package:config("shared") and "OFF" or "ON"))
+        table.insert(configs, "-DPOLYHOOK_BUILD_STATIC_RUNTIME=OFF") 
+        
+        if package:is_plat("windows") then
             table.insert(configs, "-DZYDIS_INCLUDE_DIR=" .. package:dep("zydis"):installdir("include"))
             table.insert(configs, "-DZYCORE_INCLUDE_DIR=" .. package:dep("zycore"):installdir("include"))
-            table.insert(configs, "-DZYCORE_LIBRARY=" .. package:dep("zycore"):installdir("libzycore.a"))
-            table.insert(configs, "-DZYDIS_LIBRARY=" .. package:dep("zydis"):installdir("libzydis.a"))
+            table.insert(configs, "-DZYCORE_LIBRARY=" .. package:dep("zycore"):installdir("lib/zycore.lib"))
+            table.insert(configs, "-DZYDIS_LIBRARY=" .. package:dep("zydis"):installdir("lib/zydis.lib"))
+        elseif package:is_plat("linux") then
+            table.insert(configs, "-DZYDIS_INCLUDE_DIR=" .. package:dep("zydis"):installdir("include"))
+            table.insert(configs, "-DZYCORE_INCLUDE_DIR=" .. package:dep("zycore"):installdir("include"))
+            table.insert(configs, "-DZYCORE_LIBRARY=" .. package:dep("zycore"):installdir("lib/libzycore.a"))
+            table.insert(configs, "-DZYDIS_LIBRARY=" .. package:dep("zydis"):installdir("lib/libzydis.a"))
         end
-        import("package.tools.cmake").install(package, configs, { packagedeps = { "zycore", "zydis" } })
+         
+        -- Set MSVC Settings 
+        if is_plat("windows") then
+            local tmp_msvc = os.getenv("MSVC")
+            os.setenv("MSVC", nil)  
+            import("package.tools.cmake").install(package, configs)
+            os.setenv("MSVC", tmp_msvc)  
+        else
+            import("package.tools.cmake").install(package, configs)
+        end 
+
         print(package:get("links"))
         package:add("links", "PolyHook_2")
         package:add("links", "asmtk")
