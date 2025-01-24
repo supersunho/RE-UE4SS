0943c4018ea5a100f0201b99ea81aae976f18219
supersunho <supersunho@gmail.com>
2025-01-24 23:48:40 +0900

    update xmake.lua

--- deps/third-repo/packages/p/polyhook_2/xmake.lua
+++ deps/third-repo/packages/p/polyhook_2/xmake.lua
@@ -4,11 +4,7 @@ package("polyhook_2")
     -- local version = os.iorun("git rev-list --count HEAD"):trim()
     -- git rev-list --count HEAD ==> 593 ==> 0.0.593
     add_versions("v0.0.593", "19e7cec8cce4a0068f6db04b6d3680c078183002") 
-    local patch_src = path.join(os.scriptdir(), "for-arm64.patch")
-    local patch_dest = path.join(package:cachedir(), "source", "polyhook_2")
-    os.cp(patch_src, patch_dest)
-    print(path.join(patch_dest, "for-arm64.patch"))
-    add_patches("v0.0.593", path.join(patch_dest, "for-arm64.patch"))
+
 
     set_sourcedir(os.scriptdir())
     
@@ -16,7 +12,14 @@ package("polyhook_2")
 
     on_install(function (package)
         local configs = {}
-
+        -- patch
+        local patch_src = path.join(os.scriptdir(), "for-arm64.patch")
+        local patch_dest = path.join(package:cachedir(), "source", "polyhook_2")
+        os.cp(patch_src, patch_dest)
+        print(path.join(patch_dest, "for-arm64.patch"))
+        -- add_patches("v0.0.593", path.join(patch_dest, "for-arm64.patch"))
+        -- /usr/bin/git apply --reject --ignore-whitespace /tmp/.xmake0/250124/patches/polyhook_2/v0.0.593/for-arm64.patch
+        os.vrun("/usr/bin/git apply --reject --ignore-whitespace %s", path.join(patch_dest, "for-arm64.patch"))
         -- Set CMake build types
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
